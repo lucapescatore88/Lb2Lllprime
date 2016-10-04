a3fefb3f563622933ef3058c656565203641f600
Julian Wishahi <julian.wishahi@cern.ch>
2016-10-04 16:42:48 +0200

    Change to findChannel to reflect changes in the FTDet numbering.

--- python/simulation/findChannel.py
+++ python/simulation/findChannel.py
@@ -7,6 +7,7 @@ from Configurables import LHCbApp, CondDB
 from GaudiPython.Bindings import gbl, AppMgr
 import GaudiPython
 import math
+import ROOT
 
 LHCbApp().DataType   = "Upgrade"
 LHCbApp().Simulation = True
@@ -14,6 +15,12 @@ CondDB().Upgrade = True
 
 LHCbApp().DDDBtag = "dddb-20160304"
 LHCbApp().CondDBtag = "sim-20150716-vc-md100"
+
+## New numbering scheme. Remove when FT60 is in nominal CondDB.
+#CondDB().addLayer(dbFile = "/eos/lhcb/wg/SciFi/Custom_Geoms_Upgrade/databases/DDDB_FT60.db", dbName = "DDDB")
+CondDB().addLayer(dbFile = "/afs/cern.ch/work/j/jwishahi/public/SciFiDev/DDDB_FT60.db", dbName = "DDDB")
+
+
 
 appMgr = AppMgr(outputlevel=4)
 det = appMgr.detSvc()
@@ -24,20 +31,24 @@ point_A = gbl.Gaudi.XYZPoint(-219.75,-1213.5+50,0)
 # TestBeam Position C in lcoal coordinates (near SiPMs)
 point_C = gbl.Gaudi.XYZPoint(-219.75,+1213.5-50,0)
 
-# Choose layer, quarter, module, mat
+# Choose station, layer, quarter, module, mat
+station_id = 1
 layer_id = 0
 quarter_id = 3
 module_id = 0
 mat_id = 0
 
-fibremats = [fibremat for fibremat in FT.fibremats() if (fibremat.layer() is layer_id and fibremat.quarter() is quarter_id and fibremat.module() is module_id)]
+#fibremodules = [fibremodule for fibremodule in FT.fibremodules() if (fibremodule.layer() is layer_id and fibremodule.quarter() is quarter_id and fibremodule.module() is module_id)]
+#
+#if len(fibremodules) != 1:
+#    print "Found more that one fibremodule passing the requirements. Please check!"
+#    exit()
+#
+#fibremodule = fibremodules[0]
+#print fibremodule
 
-if len(fibremats) != 1:
-    print "Found more that one fibremat passing the requirements. Please check!"
-    exit()
-
-fibremat = fibremats[0]
-print fibremat
+target_channel = gbl.LHCb.FTChannelID(station_id, layer_id, quarter_id, module_id, 0, 0)
+fibremodule = FT.findModule(target_channel)
 
 points = {
            "A": point_A,
@@ -48,37 +59,36 @@ for pos_name, pos_point in points.iteritems():
     print("===============================================================================")
     print("Getting info for testbeam position "+pos_name+".")
     point = pos_point
-    global_point = fibremat.geometry().toGlobal(point)
+    global_point = fibremodule.geometry().toGlobal(point)
     
     print "In the local frame the position is  ", point.x(), point.y(), point.z()
     print "In the global frame the position is ", global_point.x(), global_point.y(), global_point.z()
     
-    mat_thickness = fibremat.fibreMatMaxZ() - fibremat.fibreMatMinZ()
+    #mat_thickness = fibremodule.fibremoduleMaxZ() - fibremodule.fibremoduleMinZ()
+    mat_thickness = 0.130
     angle = 0 #10/180*3.14
     
     hit = gbl.LHCb.MCHit()
     hit_point_local = gbl.Gaudi.XYZPoint(point.x()-math.tan(angle)*mat_thickness/2.,point.y(),point.z()-(mat_thickness/2.)) 
-    hit_point_global = fibremat.geometry().toGlobal(hit_point_local)
+    hit_point_global = fibremodule.geometry().toGlobal(hit_point_local)
     hit.setEntry(hit_point_global)
     
     disp = gbl.Gaudi.XYZVector(math.sin(angle),0.,mat_thickness)
     hit.setDisplacement(disp)
     
-    FTPair = gbl.std.pair(gbl.LHCb.FTChannelID,'double')
-    ftpair = FTPair()
-    
-    fibremat.calculateMeanChannel(hit,ftpair)
-    channel = ftpair.first
-    fraction = ftpair.second
+    fraction = ROOT.Double(0.0) # needed fro pass-by-ref of doubles
+    channel = fibremodule.calculateChannelAndFrac(hit_point_local.x(), fraction) 
 
     print "Hit position = ",hit.entry().x(), hit.entry().y(),hit.entry().z()
-    print "Hit channel: layer=",channel.layer(),
+    print "Hit channel:"
+    print " station =", channel.station(),
+    print " layer =", channel.layer(),
     print " quarter =", channel.quarter(),    
     print " module =",channel.module(),
-    print " mat =",channel.mat(),
-    print " sipm_id =",channel.sipmId(),
-    print " cell_id =",channel.sipmCell(),
-    print " channel_id =",channel.channelID()
+    #print " mat =",channel.mat(),
+    #print " sipm_id =",channel.sipmId(),
+    print " sipm_id =",channel.sipm(),
+    print " channel_id =",channel.channel()
     print "   fractional position w.r.t. center of channel=",fraction
 
 
